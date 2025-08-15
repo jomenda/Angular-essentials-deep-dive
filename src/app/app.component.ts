d09886c1bf3dafbdda2cc9fc33b68fd9bb34e737
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-15 15:33:12 +0300

    created user component + styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,8 @@ import { HeaderComponent } from './header/header.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
