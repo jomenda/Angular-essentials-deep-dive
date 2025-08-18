81e4e57a06c8debce54fb501ae34393d010f4d18
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-18 17:12:10 +0300

    introduced signals for internal state management

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, computed, signal } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
 const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
@@ -11,14 +11,19 @@ const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  selectedUser = DUMMY_USERS[randomIndex];
+  //Signals Are Trackable Data Containers.
+  //A signal is an object that strores a value(any type of value, including nested objects).
+  //Signals are reactive, meaning that when the value changes, the UI updates automatically.
+  //Signals are used to manage state in Angular applications.
+  selectedUser = signal(DUMMY_USERS[randomIndex]);
+  imagePath = computed(() => `assets/users/${this.selectedUser().avatar}`);
 
-  get imagePath(): string {
-    return `assets/users/${this.selectedUser.avatar}`;
-  }
+  // get imagePath(): string {
+  //   return `assets/users/${this.selectedUser.avatar}`;
+  // }
 
   onSelectUser() {
     const randomIndex = Math.floor(Math.random() * DUMMY_USERS.length);
-    this.selectedUser = DUMMY_USERS[randomIndex];
+    this.selectedUser.set(DUMMY_USERS[randomIndex]);
   }
 }
