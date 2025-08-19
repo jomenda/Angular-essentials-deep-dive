a73556ad407b671cd4e6e81f83126ffa3f40636b
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-19 15:29:20 +0300

    Created tasks component + handled dynamic selection and rendering of data

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -5,7 +5,7 @@ import {
   Input,
   input,
   Output,
-  output
+  output,
 } from '@angular/core';
 
 @Component({
@@ -19,8 +19,8 @@ export class UserComponent {
   @Input({ required: true }) id!: string;
   @Input({ required: true }) avatar!: string;
   @Input({ required: true }) name!: string;
-  // @Output() select = new EventEmitter();
-  select = output<string>()
+  @Output() select = new EventEmitter<string>();
+  // select = output<string>()
 
   get imagePath() {
     return 'assets/users/' + this.avatar;
