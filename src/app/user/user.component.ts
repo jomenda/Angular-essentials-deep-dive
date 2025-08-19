0fda275b65c0feb247571dbfe864f938a6ad269c
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-19 10:05:31 +0300

    iMPLEMENTED eVENT EMITTING + oUTPUT DECORATOR

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,4 +1,11 @@
-import { Component, computed, Input, input } from '@angular/core';
+import {
+  Component,
+  computed,
+  EventEmitter,
+  Input,
+  input,
+  Output,
+} from '@angular/core';
 
 @Component({
   selector: 'app-user',
@@ -8,19 +15,17 @@ import { Component, computed, Input, input } from '@angular/core';
   styleUrl: './user.component.css',
 })
 export class UserComponent {
-  // @Input({ required: true }) avatar!: string;
-  // @Input({ required: true }) name!: string;
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter();
 
-  avatar = input.required<string>();
-  name = input.required<string>();
-
-  // get imagePath() {
-  //   return 'assets/users/' + this.avatar;
-  // }
-  
-  imagePath = computed(() => 'assets/users/' + this.avatar());
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
 
   onSelectUser() {
     console.log('User Selected');
+    this.select.emit(this.id);
   }
 }
