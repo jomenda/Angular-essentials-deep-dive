5e12589096afbba402c8969b04f7f361cf9ea49b
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-20 17:14:49 +0300

    Implemented use of Directives and two way binding

--- src/app/tasks/addtask/addtask.component.ts
+++ src/app/tasks/addtask/addtask.component.ts
@@ -1,15 +1,18 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './addtask.component.html',
   styleUrl: './addtask.component.css',
 })
 export class AddtaskComponent {
-
-   @Output() cancel = new EventEmitter<void>()
+  @Output() cancel = new EventEmitter<void>();
+  enterredTitle = '';
+  enterredSummary = '';
+  enteredDueDate = '';
 
   onCancel() {
     this.cancel.emit();
