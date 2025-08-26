4b11ebaf1e3a442d62e2af9e332a0afac17571dd
James Omenda <James.Omenda@ccieastafrica.com>
2025-08-26 16:47:18 +0300

    Using localStirage for Data Storage

--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -29,6 +29,17 @@ class TasksService {
     },
   ];
 
+  /**
+   *
+   */
+  constructor() {
+    const tasks = localStorage.getItem('tasks');
+
+    if (tasks) {
+      this.tasks = JSON.parse(tasks);
+    }
+  }
+
   getUserTasks(userId: string) {
     return this.tasks.filter((task) => task.userId === userId);
   }
@@ -41,10 +52,16 @@ class TasksService {
       summary: task.summary,
       dueDate: task.date,
     });
+    this.saveTasks();
   }
 
   removeTask(id: string) {
     this.tasks = this.tasks.filter((task) => task.id != id);
+    this.saveTasks();
+  }
+
+  private saveTasks() {
+    localStorage.setItem('tasks', JSON.stringify(this.tasks));
   }
 }
 
